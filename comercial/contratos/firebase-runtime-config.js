87df98f98c8b4bc8ca07c093f2e8a024b83133e7
Vavaasz <133982458+Vavaasz@users.noreply.github.com>
2026-06-24 11:51:06 -0300

    Disable rejected mirror content

--- comercial/contratos/firebase-runtime-config.js
+++
@@ -1,8 +0,0 @@
-window.TKA_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBnIO49n4-7TsJiHW1QUR1yk19BZ-xHH-w",
-  authDomain: "cadastro-clientes-tka.firebaseapp.com",
-  projectId: "cadastro-clientes-tka",
-  storageBucket: "cadastro-clientes-tka.firebasestorage.app",
-  messagingSenderId: "595783056780",
-  appId: "1:595783056780:web:4d614cd834976dab296aa9"
-};
--- whatsapp-bridge/install.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-setlocal
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0install-on-this-computer.ps1"
-pause
